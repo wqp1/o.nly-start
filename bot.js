39b201a7ef2de9fd152751a12cb44653cb41b15d
iRealKsA <48433959+iRealKsA@users.noreply.github.com>
2019-03-31 02:07:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1809,4 +1809,19 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', message => {
+if(message.content.startsWith("-slots")) {
+  let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+  let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let we;
+  if(slots1 === slots2 && slots2 === slots3) {
+    we = "Win!"
+  } else {
+    we = "Lose!"
+  }
+  message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});//iRealKsA
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
